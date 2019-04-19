3babd45525627d888226510052d14fe3b4b80e1a
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2019-04-19 21:28:15 +0200

    add ui for matchesFragment

--- app/src/main/java/com/example/mahmoud_ashraf/koraapp/fragments/MatchesFragment.kt
+++ app/src/main/java/com/example/mahmoud_ashraf/koraapp/fragments/MatchesFragment.kt
@@ -2,10 +2,8 @@ package com.example.mahmoud_ashraf.koraapp.fragments
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.example.mahmoud_ashraf.koraapp.R
 
@@ -22,6 +20,17 @@ class MatchesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_matches, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.main, menu)
     }
 
 
